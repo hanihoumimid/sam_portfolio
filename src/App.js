2ba594edd8f7bac02cb04bab8cdebba6f5ee0232
Hani Houmimid <hani.houmimid@edu.univ-paris13.fr>
2025-04-03 05:16:02 +0200

    Premier commit, on commence le portfolio de Sam avec react

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>Hey, I'm Sam PAROT! </h1>
       </header>
+
+      <h2>Welcome to my portfolio! Here you will find all the informations about me, whether it's personal or about my work. Enjoy! ✨</h2>
+
+      <p>Let's discover my work ✨</p>
+      <a href="mailto:sam.parotech@gmail.com">sam.parotech@gmail.com</a>
+      <br />
+      <a href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ1LNlHXLcvGbZW4TmGZH0xQFQeslBokQ4awx0lWJBnqFgRo55wa8dK9OQN-NKmURwXrOpC-KChj">Book a call</a>
     </div>
   );
 }
