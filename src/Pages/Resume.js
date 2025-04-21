a0bd5a081ca1443cb0286b091a4b9209e7b28a23
Hani <hhoumimid@gmail.com>
2025-04-21 17:49:37 +0200

    partie introduction du resume terminée

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,4 +1,13 @@
 import React, { useState } from 'react';
+import pp from '../assets/sam_pp.webp'
+// import fond from '../assets/etoiles.webp';
+
+
+// style={{
+//   backgroundImage: `url(${fond})`,
+//   backgroundSize: 'cover',
+//   backgroundPosition: 'center',
+// }}
 
 const Resume = () => {
   const [visibleSection, setVisibleSection] = useState(null);
@@ -19,6 +28,51 @@ const Resume = () => {
           <p className="mt-6 text-xl leading-8 text-gray-200">
             Here's a quick overview of my journey and skillset.
           </p>
+        </div>
+
+        {/*Introduction*/}
+
+        <div className="flex flex-row items-start gap-10 px-6 py-4">
+          <img 
+            src={pp} 
+            alt="Sam's pic" 
+            className="border border-black rounded-lg w-60 h-60 object-cover" 
+          />
+          
+          <p className="text-xl text-white font-semibold leading-relaxed">
+            Hi, I’m Sam — currently a student at the National Aeronautical School and an apprentice designer.<br />
+            I’ve been studying aircraft for the past three years and am passionate about design. I aspire to become a designer in the future.<br />
+            Always on the lookout for new adventures — whether it’s traveling or engaging in exciting job opportunities — I bring motivation and dedication to everything I do.<br />
+            I’m a French native, fluent in English, and through my various experiences, I’ve developed strong teamwork skills, active listening abilities, and effective communication.
+          </p>
+
+          <ul className="space-y-6">
+            <p className="text-white">Contact me on :</p>
+            <li  className="border-l border-white/10 pl-6 text-white">
+              <a href='mailto:sam.parotech@gmail.com' alt="mail sam">sam.parotech@gmail.com</a>
+            </li>
+            <li  className="border-l border-white/10 pl-6 text-white">
+              <a href="tel:+33 7 81 87 29 38 " alt="french number">+33 7 81 87 29 38 </a>
+            </li>
+            <li  className="border-l border-white/10 pl-6 text-white">
+              <a href="tel:+1 (450) 626-8577" alt="canadian number">+1 (450) 626-8577 </a>
+            </li>
+          </ul>
+        </div>
+
+
+
+        {/*About me*/}
+
+        <div className="space-y-12">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">About me</h2>
+          <ul className="space-y-6">
+            <li className="border-l border-white/10 pl-6">
+              <p className="text-gray-300">
+
+              </p>
+            </li>
+          </ul>
         </div>
 
         {/* Formations */}
